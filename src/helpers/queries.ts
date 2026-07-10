cf9b3e355ac2412c6404569b00c7beaf9da0e43a
Nair Paez <nairpaez@outlook.com>
2026-07-10 10:12:21 -0300

    agragamos el archivo de queries para las consultas para listar y crear un atarea

---
+++ src/helpers/queries.ts
@@ -0,0 +1,28 @@
+
+const urlTareas = import.meta.env.VITE_TAREAS
+
+export const listarTareasApi = async ():Promise<Response> =>{
+    try{
+        const respuesta = await fetch(urlTareas)
+        return respuesta
+    }catch(error){
+        console.error(error)
+        throw error
+    }
+};
+
+export const crearTareaApi = async (tarea: String):Promise<Response> =>{
+    try{
+        const respuesta = await fetch(urlTareas, {
+            method: 'POST',
+            headers:{
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(tarea)
+        })
+        return respuesta
+    }catch(error){
+        console.error(error)
+        throw error
+    }
+};
